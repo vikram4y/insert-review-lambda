8c2ea534d5736f16f2814910295abf66f755be2a
vikram <vikram4y@gmail.com>
2024-10-19 19:39:42 +0530

    changes

--- src/main/java/com/reviews/insert/InsertReviewLambda.java
+++ src/main/java/com/reviews/insert/InsertReviewLambda.java
@@ -7,7 +7,10 @@ import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class InsertReviewLambda implements RequestHandler<Map<String, Object>, String> {
 
@@ -19,16 +22,21 @@ public class InsertReviewLambda implements RequestHandler<Map<String, Object>, S
         initializeDynamoDB();
 
         String productId = (String) input.get("product_id");
-        String reviewText = (String) input.get("review_text");
+        List<String> reviewList = (List<String>) input.get("review_texts");
 
         Table table = dynamoDB.getTable(TABLE_NAME);
 
-        table.putItem(new Item()
-                .withPrimaryKey("product_id", productId)
-                .withString("review_text", reviewText));
+        for (String reviewText : reviewList) {
+            String reviewId = UUID.randomUUID().toString();
+            table.putItem(new Item()
+                    .withPrimaryKey("product_id", productId,
+                            "review_id", reviewId)
+                    .withString("review_text", reviewText));
+        }
 
-        return "Review inserted successfully!";
+        return "Reviews inserted successfully!";
     }
+
 
     private void initializeDynamoDB() {
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
